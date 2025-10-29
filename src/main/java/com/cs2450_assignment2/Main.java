4ab127c4dcf3f3f4f5a0ca99ba6973980cf1f548
Jeannette Ruiz <151603603+jeanrnette@users.noreply.github.com>
2025-10-28 21:43:49 -0700

    Change FXML layout from Layout2 to Layout3

--- src/main/java/com/cs2450_assignment2/Main.java
+++ src/main/java/com/cs2450_assignment2/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-       Parent root = FXMLLoader.load(getClass().getResource("/com/cs2450_assignment2/Layout2.fxml"));
+       Parent root = FXMLLoader.load(getClass().getResource("/com/cs2450_assignment2/Layout3.fxml"));
 
         Scene scene = new Scene(root, 300, 275);
     
